2a41f043307fd2040f561c656a805f62490d2bb5
nbd-oz <ljh12@torder.com>
2024-12-20 16:24:16 +0900

    feat: strike 작업 중

--- src/main/java/Baseball.java
+++ src/main/java/Baseball.java
@@ -5,13 +5,12 @@ public class Baseball {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int randomNumber = ThreadLocalRandom.current().nextInt(111, 1000);
-
-        String ramdomString = Integer.toString(randomNumber);
         System.out.println("random number: " + randomNumber);
 
         System.out.println("숫자를 입력하세요");
         String inputString = scanner.nextLine();
 
+        String ramdomString = Integer.toString(randomNumber);
         int strikeCount = calcStrike(ramdomString, inputString);
 
         System.out.println(inputString);
@@ -23,6 +22,11 @@ public class Baseball {
         String[] numberSplit = ramdomString.split("");
         String[] inputSplit = inputNumber.split("");
 
+        count = teset(numberSplit, inputSplit, count);
+        return count;
+    }
+
+    private static int teset(String[] numberSplit, String[] inputSplit, int count) {
         for (int i = 0; i < numberSplit.length; i++) {
             if (numberSplit[i].equals(inputSplit[i])) {
                 count++;
